c310e7cfd0e46db1a2d595b573367267f1d3c5cd
Sam Skulsky <sam_skulsky@icloud.com>
2023-09-07 08:39:02 -0700

    Init

--- Index.java
+++ Index.java
@@ -1,7 +1,17 @@
+import java.io.File;
 import java.io.IOException;
 
 public class Index {
-    public static void main(String[] args) throws IOException {
-        Blob blob = new Blob("testfile.txt");
+    public static void init() throws IOException {
+        File in = new File("index");
+        File obj = new File("objects");
+
+        if (!in.isFile()) {
+            in.createNewFile();
+        }
+
+        if (!obj.isDirectory()) {
+            obj.mkdir();
+        }
     }
 }
