e6238953f1d4b698a0794d4556e32d3a0a90c3a5
Sam Skulsky <sam_skulsky@icloud.com>
2023-09-07 08:25:39 -0700

    Blob

---
+++ Blob.java
@@ -0,0 +1,47 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Blob {
+
+    public Blob(String path) throws IOException {
+        String fileData = readFile(path);
+
+        String sha1 = toSha1(fileData);
+
+        FileWriter fw = new FileWriter("objects/" + sha1, false);
+        fw.write(fileData);
+        fw.close();
+    }
+
+    public String readFile(String from) throws IOException {
+        return Files.readString(Path.of(from));
+    }
+
+    public String toSha1(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            String hashtext = no.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ Index.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class Index {
+    public static void main(String[] args) throws IOException {
+        Blob blob = new Blob("testfile.txt");
+    }
+}
