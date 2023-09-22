4026182688183c9b9648066ea9c4d37cf186541e
Kian Sharifi <kiansharifi88@gmail.com>
2023-09-21 21:59:09 -0700

    Worked on commit and commitTester
    Forgot to push earlier progress myb

--- BlobTest.java
+++ BlobTest.java
@@ -2,73 +2,82 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-
 public class BlobTest {
-private static final String testFinalPath = "testfile2.txt";
-private static final String testFileContent = "Testing the index and blob classes";
-    
-    @Test
-    public void testBlob() throws IOException {
+    private static final String testFinalPath = "testfile2.txt";
+    private static final String testFileContent = "Testing the index and blob classes";
 
-        
-        Blob blob = new Blob(testFinalPath);
-
-        
-        assertNotNull(blob.getSha1());
-
-        
-        assertTrue(checkFileExists("objects/" + blob.getSha1()));
-
-        
-        String originalContent = readFileContent(testFinalPath);
-
-        
-        String blobContent = readFileContent("objects/" + blob.getSha1());
-
-        
-        assertEquals(originalContent, blobContent);
-    }
-    @Test
-    void testGetSha1() {
-        String sha1 = null;
+    //create festfile2.txt and testFileContent in a BeforeAll method
+    @BeforeAll
+    static public void createFile () throws IOException
+    {
+        FileWriter fw = new FileWriter (testFinalPath);
         try
         {
-            Blob testBlob = new Blob (testFinalPath);
-            String sha = testBlob.getSha1();
-            sha1 = sha;
+            fw.write (testFileContent);
         }
         catch (IOException e)
         {
             e.printStackTrace();
         }
-        assertNotNull(sha1);
+        fw.close();
         
+    }
+
+    @Test
+    public void testBlob() throws IOException {
+
+        Blob blob = new Blob(testFinalPath);
+
+        assertNotNull(blob.getSha1());
+
+        assertTrue(checkFileExists("objects/" + blob.getSha1()));
+
+        String originalContent = readFileContent(testFinalPath);
+
+        String blobContent = readFileContent("objects/" + blob.getSha1());
+
+        assertEquals(originalContent, blobContent);
+    }
+
+    @Test
+    void testGetSha1() {
+        String sha1 = null;
+        try {
+            Blob testBlob = new Blob(testFinalPath);
+            String sha = testBlob.getSha1();
+            sha1 = sha;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertNotNull(sha1);
+
     }
 
     @Test
     void testToSha1() {
         String sha1 = null;
-        try
-        {
-            Blob blob = new Blob (testFinalPath);
-           sha1 = blob.toSha1 (testFileContent);
+        try {
+            Blob blob = new Blob(testFinalPath);
+            sha1 = blob.toSha1(testFileContent);
 
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         assertNotNull(sha1);
     }
-     private boolean checkFileExists(String filePath) {
+
+    private boolean checkFileExists(String filePath) {
         return Files.exists(Paths.get(filePath));
     }
+
     private String readFileContent(String filePath) throws IOException {
         return new String(Files.readAllBytes(Paths.get(filePath)));
     }
---
+++ Commit.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Commit {
+    private String treeSha1;
+    private String parentSha1;
+    private String author;
+    private Date date;
+    private String summary;
+
+    public Commit(String parentSha1, String author, String summary) {
+        Tree tree = new Tree();
+        this.treeSha1 = tree.getSha1();
+        this.parentSha1 = parentSha1;
+        this.author = author;
+        this.date = new Date(); // Current date and time
+        this.summary = summary;
+    }
+
+    public String getTreeSha1() {
+        return treeSha1;
+    }
+
+    public String getParentSha1() {
+        return parentSha1;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM d, yyyy");
+        return dateFormat.format(date);
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public String generateSha1() throws NoSuchAlgorithmException {
+        StringBuilder commitData = new StringBuilder();
+        commitData.append(treeSha1 + "\n");
+        commitData.append(parentSha1 + "\n");
+        commitData.append("\n");
+        commitData.append(author + "\n");
+        commitData.append(getDate() + "\n");
+        commitData.append(summary + "\n");
+
+        String commitDataString = commitData.toString();
+
+        MessageDigest digest = MessageDigest.getInstance("SHA-1");
+        byte[] sha1Bytes = digest.digest(commitDataString.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder sha1Hex = new StringBuilder();
+        for (byte b : sha1Bytes) {
+            sha1Hex.append(String.format("%02x", b));
+        }
+
+        return sha1Hex.toString();
+    }
+
+    public void writeToFile(String fileName) throws IOException, NoSuchAlgorithmException {
+        String sha1 = generateSha1();
+        String filePath = "objects/" + sha1;
+
+        StringBuilder commitData = new StringBuilder();
+        commitData.append(treeSha1 + "\n");
+        commitData.append(parentSha1 + "\n");
+        commitData.append("\n");
+        commitData.append(author + "\n");
+        commitData.append(getDate() + "\n");
+        commitData.append(summary + "\n");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(commitData.toString());
+        }
+    }
+}
